bf9393d608622b058bfb1a185f496382ead03a21
zdebar <157598603+zdebar@users.noreply.github.com>
2025-02-09 17:51:09 +0100

    Typescript Head

---
+++ my-next-app/pages/_app.tsx
@@ -0,0 +1,18 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import '../styles/globals.css';
+import "../styles/index.css";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Zdeněk Barth</title>
+        <meta charSet="UTF-8" />  
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" /> 
+        <meta name="description" content="Zdeněk Barth's personal website" /> 
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
