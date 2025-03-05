7789fe41d0da4ef0d7fc5b437e7105f7c425c0e0
zdebar <157598603+zdebar@users.noreply.github.com>
2025-03-05 18:13:18 +0100

    new next folder structure

--- my-next-app/pages/_document.tsx
+++ my-next-app/pages/_document.tsx
@@ -1,26 +1,22 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document';
 
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>        
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-          
-          <link
-            href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
-            rel="stylesheet"
-          />
-          <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function MyDocument() {
+  return (
+    <Html lang="en">
+      <Head>        
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        
+        <link
+          href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+          rel="stylesheet"
+        />
+        <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export default MyDocument;
---
+++ my-next-app/src/app/layout.tsx
@@ -0,0 +1,30 @@
+import Head from "next/head";
+import "../styles/globals.css";
+import "../styles/index.css";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export const metadata = {
+  title: "Zdeněk Barth",
+  description: "Zdeněk Barth's personal website",
+  keywords: "programování, web development, react, next.js",
+  authors: [{ name: "Zdeněk Barth" }],
+  viewport: "width=device-width, initial-scale=1.0",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <Head>
+          <meta charSet="UTF-8" />
+        </Head>
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
+
+
